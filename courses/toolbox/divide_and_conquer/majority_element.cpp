b4ecf7f1756783614ef2dc20f318e80af554534e
rproskuryakov <rodion43pv@gmail.com>
2020-12-06 02:45:53 +0700

    Implement majority element (toolbox 4th week) algorithm

--- courses/toolbox/divide_and_conquer/majority_element.cpp
+++ courses/toolbox/divide_and_conquer/majority_element.cpp
@@ -1,13 +1,12 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
 
 #include "majority_element.h"
 
-int count_element(std::vector<int> &a, int majority_cand, int left, int right) {
+int count_element(std::vector<int> &a, int elem, int left, int right) {
     int count = 0;
-    for (size_t i = left; left < right; left++) {
-        if (a[i] == majority_cand) {
+    for (size_t i = left; i < right; ++i) {
+        if (a[i] == elem) {
             ++count;
         }
     }
@@ -15,34 +14,47 @@ int count_element(std::vector<int> &a, int majority_cand, int left, int right) {
 }
 
 int recursive_majority_element(std::vector<int> &a, int left, int right) {
-    if (right == left) {
+    if (left == right - 1) {
         return a[left];
     }
 
-    int middle = (int) ((left + right) / 2);
+    int middle = (int)((right + left) / 2);
     int left_majority = recursive_majority_element(a, left, middle);
     int right_majority = recursive_majority_element(a, middle, right);
-
     if (left_majority == right_majority) {
         return left_majority;
     }
-    int left_count = count_element(a, left_majority, left, middle);
-    int right_count = count_element(a, right_majority, middle, right);
-    return (left_count < right_count) ? left_count : right_count;
+    int left_count = count_element(a, left_majority, left, right);
+    int right_count = count_element(a, right_majority, left, right);
+    return (left_count > right_count) ? left_majority : right_majority;
 }
 
 int get_majority_element(std::vector<int> &a, int left, int right) {
-    //int majority = recursive_majority_element(a, left, right);
-    return -1;
+    if (left == right - 1) {
+        return 1;
+    }
+
+    int middle = (int) ((right + left) / 2);
+    int left_majority = recursive_majority_element(a, left, middle);
+    int right_majority = recursive_majority_element(a, middle, right);
+    int left_count = count_element(a, left_majority, left, right);
+    int right_count = count_element(a, right_majority, left, right);
+    if (left_count > ((right - left) / 2)) {
+        return 1;
+    }
+    if (right_count > ((right - left) / 2)) {
+        return 1;
+    }
+    return 0;
 }
 
 int majority_element_main() {
     int n;
     std::cin >> n;
     std::vector<int> a(n);
-    for (int & i : a) {
+    for (int &i : a) {
         std::cin >> i;
     }
-    std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+    std::cout << get_majority_element(a, 0, a.size()) << '\n';
     return 0;
 }
--- tests/toolbox/divide_and_conquer/majority_element_test.cpp
+++ tests/toolbox/divide_and_conquer/majority_element_test.cpp
@@ -2,80 +2,79 @@
 
 #include "toolbox/divide_and_conquer/majority_element.h"
 
-TEST(MajorityElementTestSuite, TaskDescriptionTestsFirst) {
+TEST(MajorityElementTestSuite, TaskDescriptionFirstExampleContainsMajorityElementTest) {
     std::vector<int> array = {2, 3, 9, 2, 2};
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 1);
 }
 
-TEST(MajorityElementTestSuite, TaskDescriptionTestsSecond) {
+TEST(MajorityElementTestSuite, TaskDescriptionSecondDoesntContainsMajorityElementTest) {
     std::vector<int> array = {1, 2, 3, 4};
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 0);
 }
 
-TEST(MajorityElementTestSuite, TaskDescriptionTestsThird) {
+TEST(MajorityElementTestSuite, TaskDescriptionThirdDoesntContainsMajorityElementTest) {
     std::vector<int> array = {1, 2, 3, 1};
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 0);
 }
 
-TEST(MajorityElementTestSuite, EmptyArray) {
-    std::vector<int> array = {};
-    EXPECT_EQ(get_majority_element(array, 0, array.size()), 0);
-}
-
-TEST(MajorityElementTestSuite, ArrayWithOneElement) {
+TEST(MajorityElementTestSuite, ArrayWithOneElementTest) {
     std::vector<int> array = {1};
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 1);
 }
 
-TEST(MajorityElementTestSuite, TwoElementsArrayWithMajorityElement) {
+TEST(MajorityElementTestSuite, TwoElementsArrayWithMajorityElementTest) {
     std::vector<int> array = {2000, 2000};
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 1);
 }
 
-TEST(MajorityElementTestSuite, TwoElementsArrayWithoutMajorityElement) {
+TEST(MajorityElementTestSuite, TwoElementsArrayWithoutMajorityElementTest) {
     std::vector<int> array = {500000, 200000};
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 0);
 }
 
-TEST(MajorityElementTestSuite, NoMajorityElementEvenNumberOfElements) {
+TEST(MajorityElementTestSuite, NoMajorityElementEvenNumberOfElementsTest) {
     std::vector<int> array = {6, 1, 4, 2, 0, 10};
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 0);
 }
 
-TEST(MajorityElementTestSuite, NoMajorityElementOddNumberOfElements) {
+TEST(MajorityElementTestSuite, NoMajorityElementOddNumberOfElementsTest) {
     std::vector<int> array = {5, 4, 2, 3, 1};
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 0);
 }
 
-TEST(MajorityElementTestSuite, NoMajorityElementHalf) {
+TEST(MajorityElementTestSuite, NoMajorityElementHalfTest) {
     std::vector<int> array = {0, 10, 1, 5, 10, 10};
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 0);
 }
 
-TEST(MajorityElementTestSuite, MaximumValueArrayWithMajorityElement) {
+TEST(MajorityElementTestSuite, MaximumValueArrayWithMajorityElementTest) {
     std::vector<int> array = {1000000000, 0, 1000000000};
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 1);
 }
 
-TEST(MajorityElementTestSuite, MaximumElementsArrayWithoutMajorityElement) {
+TEST(MajorityElementTestSuite, MaximumElementsArrayWithoutMajorityElementTest) {
     std::vector<int> array(100000);
     for (size_t i = 0; i < 50000; ++i) {
-        array.push_back(5);
+        array[i] = 5;
     }
-    for (size_t i = 0; i < 50000; ++i) {
-        array.push_back(10);
+    for (size_t i = 50000; i < 100000; ++i) {
+        array[i] = 10;
     }
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 0);
 }
 
-TEST(MajorityElementTestSuite, MaximumElementsArrayWithMajorityElement) {
+TEST(MajorityElementTestSuite, MaximumElementsArrayWithMajorityElementTest) {
     std::vector<int> array(100000);
-    for (size_t i = 0; i <= 50001; ++i) {
-        array.push_back(5);
+    for (size_t i = 0; i < 50001; ++i) {
+        array[i] = 5;
     }
-    for (size_t i = 0; i <= 49999; ++i) {
-        array.push_back(10);
+    for (size_t i = 50001; i < 100000; ++i) {
+        array[i] = 10;
     }
     EXPECT_EQ(get_majority_element(array, 0, array.size()), 1);
 }
 
+TEST(MajorityElementTestSuite, SecondTest) {
+    std::vector<int> array = {512766168, 717383758, 5, 126144732, 5, 573799007, 5, 5, 5, 405079772};
+    EXPECT_EQ(get_majority_element(array, 0, array.size()), 0);
+}
