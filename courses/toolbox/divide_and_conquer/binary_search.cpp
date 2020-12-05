f537feac992ef840e55ac236b7ca4d9f08b7511c
rproskuryakov <rodion43pv@gmail.com>
2020-12-06 02:43:45 +0700

    Implement binary search (toolbox 4th week) algorithm

--- courses/toolbox/divide_and_conquer/binary_search.cpp
+++ courses/toolbox/divide_and_conquer/binary_search.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
-#include <cassert>
 #include <vector>
 
 #include "binary_search.h"
 
-using std::vector;
 
-int binary_search(const vector<int> &a, int x) {
-//    int left, right;
-//    left = 0;
-//    right = a.size();
-//    while (left <= right) {
-//        long middle = (left + (right - left) / 2);
-//        if (x == a[middle]) {
-//            return middle;
-//        } else if (x < a[middle]) {
-//            right = middle - 1;
-//        } else {
-//            left = middle + 1;
-//        }
-//    }
-    return -10;
+int binary_search(const std::vector<int> &a, int x) {
+    int left, right;
+    left = 0;
+    right = a.size() - 1;
+    while (left <= right) {
+        int middle = left + (int)((right - left) / 2);
+        if (x == a[middle]) {
+            return middle;
+        } else if (x < a[middle]) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+    return -1;
 }
 
-int linear_search(const vector<int> &a, int x) {
+int linear_search(const std::vector<int> &a, int x) {
     for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] == x) return i;
     }
@@ -33,18 +31,17 @@ int linear_search(const vector<int> &a, int x) {
 int binary_search_main() {
     int n;
     std::cin >> n;
-    vector<int> a(n);
+    std::vector<int> a(n);
     for (int & i : a) {
         std::cin >> i;
     }
     int m;
     std::cin >> m;
-    vector<int> b(m);
+    std::vector<int> b(m);
     for (int i = 0; i < m; ++i) {
         std::cin >> b[i];
     }
     for (int i = 0; i < m; ++i) {
-        //replace with the call to binary_search when implemented
         std::cout << binary_search(a, b[i]) << ' ';
     }
     return 0;
--- tests/toolbox/divide_and_conquer/binary_search_test.cpp
+++ tests/toolbox/divide_and_conquer/binary_search_test.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+
 #include "toolbox/divide_and_conquer/binary_search.h"
 
 
@@ -9,26 +10,46 @@ TEST(BinarySearchTestSuite, TaskDescriptionFirstTest) {
 }
 
 TEST(BinarySearchTestSuite, TaskDescriptionSecondTest) {
-    std::vector<int> a = {5, 1, 5, 8, 12, 13};
+    std::vector<int> a = {1, 5, 8, 12, 13};
     int answer = binary_search(a, 1);
     EXPECT_EQ(answer, 0);
 }
 
 TEST(BinarySearchTestSuite, TaskDescriptionThirdTest) {
-    std::vector<int> a = {5, 1, 5, 8, 12, 13};
+    std::vector<int> a = {1, 5, 8, 12, 13};
     int answer = binary_search(a, 23);
     EXPECT_EQ(answer, -1);
 }
 
 TEST(BinarySearchTestSuite, TaskDescriptionFourthTest) {
-    std::vector<int> a = {5, 1, 5, 8, 12, 13};
+    std::vector<int> a = {1, 5, 8, 12, 13};
     int answer = binary_search(a, 11);
     EXPECT_EQ(answer, -1);
 }
 
 TEST(BinarySearchTestSuite, NoValueInVector) {
     std::vector<int> a = {1, 2, 3, 4, 5};
-    long answer = binary_search(a, 10);
+    int answer = binary_search(a, 10);
     EXPECT_EQ(answer, -1);
 }
+
+TEST(BinarySearchTestSuite, MaximumElementsArrayTest) {
+    std::vector<int> array(30000);
+    for (size_t i = 0; i < 30000; ++i) {
+        array[i] = i * i;
+    }
+    EXPECT_EQ(binary_search(array, 500 * 500), 500);
+    EXPECT_EQ(binary_search(array, 29999 * 29999), 29999);
+    EXPECT_EQ(binary_search(array, 50000000), -1);
+    EXPECT_EQ(binary_search(array, 1000000000), -1);
+    EXPECT_EQ(binary_search(array, 999999999), -1);
+}
+
+TEST(BinarySearchTestSuite, OneElementArray) {
+    std::vector<int> array = {1, 2, 3};
+    EXPECT_EQ(binary_search(array, 3), 2);
+    EXPECT_EQ(binary_search(array, 4), -1);
+}
+
+
 
